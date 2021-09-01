1baf1dadd3449a723af1e0cc938a9b2c78fe9db9
dee7050 <tdk7011@ro.ru>
2021-09-01 23:10:09 +0300

    add dir and files

---
+++ src/test/java/TextBoxTst.java
@@ -0,0 +1,10 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TextBoxTst {
+}
